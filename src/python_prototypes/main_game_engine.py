6c409b949568a7130ea0f2145da9f7f6e9f68b09
patex1987 <patex1987@gmail.com>
2025-01-13 12:46:34 +0100

    [python_idea] logic for finding the next steps added

--- src/python_prototypes/main_game_engine.py
+++ src/python_prototypes/main_game_engine.py
@@ -1,5 +1,5 @@
 from python_prototypes.field_types import GameGridInformation, PlayerState, GRID_COORD_UNIT_STATE_T, GridUnitState
-from python_prototypes.reaper.decision_maker import reaper_decider
+from python_prototypes.reaper.decision_maker import reaper_decider, ReaperDecisionType
 from python_prototypes.reaper.input_to_q_state import calculate_reaper_q_state
 from python_prototypes.reaper.q_orchestrator import ReaperGameState
 
@@ -30,6 +30,31 @@ class MainGameEngine:
         enemy_rage_1: int,
         enemy_rage_2: int,
     ):
+        """
+        Handles the raw data incoming rom every round in the game
+
+        :param enemy_others_grid_state:
+        :param enemy_others_id_to_grid_coord:
+        :param enemy_reaper_grid_state:
+        :param enemy_reaper_id_to_grid_coord:
+        :param full_grid_state:
+        :param my_rage:
+        :param my_score:
+        :param player_destroyer_grid_unit:
+        :param player_doof_grid_unit:
+        :param player_reaper_grid_unit:
+        :param tanker_grid_state:
+        :param tanker_id_to_grid_coord:
+        :param wreck_grid_state:
+        :param wreck_id_to_grid_coord:
+        :param enemy_score_1:
+        :param enemy_score_2:
+        :param enemy_rage_1:
+        :param enemy_rage_2:
+        :return:
+
+        TODO: use the enemy score and rage values similarly to player state
+        """
         game_grid_information = GameGridInformation(
             full_grid_state=full_grid_state,
             wreck_grid_state=wreck_grid_state,
@@ -54,9 +79,42 @@ class MainGameEngine:
         reaper_q_state = calculate_reaper_q_state(
             game_grid_information=game_grid_information, player_state=player_state
         )
+        original_target = self.reaper_game_state.current_target_info
+        original_mission_steps = self.reaper_game_state._mission_steps
+
+        # TODO: make reaper_decider oop based and add it as a class field,
+        #   so you can inject a different implementation in your tests
         reaper_decision = reaper_decider(
             reaper_game_state=self.reaper_game_state,
             reaper_q_state=reaper_q_state,
             game_grid_information=game_grid_information,
             player_state=player_state,
         )
+
+        match reaper_decision.decision_type:
+            case ReaperDecisionType.existing_target:
+                strategy_path = self.reaper_game_state._planned_game_output_path
+            case ReaperDecisionType.replan_existing_target:
+                strategy_path = ReaperPathPlanner.plan(
+                    reaper_decision.goal_action_type, reaper_decision.target_grid_unit
+                )
+            case (
+                ReaperDecisionType.new_target_on_failure
+                | ReaperDecisionType.new_target_on_success
+                | ReaperDecisionType.new_target_on_undefined
+            ):
+                long_term_tracker = None
+                if reaper_decision.decision_type == ReaperDecisionType.new_target_on_success:
+                    long_term_tracker = get_success_long_term_tracker(original_mission_steps)
+                if reaper_decision.decision_type == ReaperDecisionType.new_target_on_failure:
+                    long_term_tracker = get_failure_long_term_tracker(original_mission_steps)
+                self.reaper_game_state.register_long_term_tracker(long_term_tracker, original_target)
+
+                strategy_path = ReaperPathPlanner.plan(
+                    reaper_decision.goal_action_type, reaper_decision.target_grid_unit
+                )
+            case _:
+                raise ValueError(f"Unknown decision type: {reaper_decision.decision_type}")
+
+        reaper_next_step = strategy_path.get_next_step()
+        print(reaper_next_step)
