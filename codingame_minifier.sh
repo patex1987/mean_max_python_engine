64620337c81e8ca5cabba9539688da81b98c2612
patex1987 <patex1987@gmail.com>
2025-10-18 23:21:18 +0200

    fully minified code created

--- codingame_minifier.sh
+++ codingame_minifier.sh
@@ -1,5 +1,5 @@
 python ./merge_to_single_file.py
 
-#uv tool run --from python-minifier pyminify merged_codingame_solution.py --output minified_codingame.py --remove-literal-statements
-uv tool run --from python-minifier pyminify merged_codingame_solution.py --output minified_codingame_larger.py --remove-literal-statements --no-rename-locals --remove-asserts --remove-debug
-black -S -l 120 ./minified_codingame_larger.py
+uv tool run --from python-minifier pyminify merged_codingame_solution.py --output minified_codingame.py --remove-literal-statements
+uv tool run --from python-minifier pyminify merged_codingame_solution.py --output minified_codingame_larger.py --remove-literal-statements --no-rename-locals --no-hoist-literals
+uv tool run ruff format --line-length 120 ./minified_codingame_larger.py
